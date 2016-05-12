4db14e8d27065a76a0f10f9e468aab61a5afd903
owen <2368855203@qq.com>
2016-05-12 10:54:28 +0800

    build.gradle 的渠道号和u盟配置

---
+++ app/src/main/java/com/owen/packapk/MainActivity.java
@@ -0,0 +1,27 @@
+package com.owen.packapk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.umeng.analytics.MobclickAgent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+}
