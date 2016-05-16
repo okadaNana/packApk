27801393ef2ada0f7a961ed7517d0a5247aa7e12
owen <2368855203@qq.com>
2016-05-16 18:13:14 +0800

    生产环境与开发环境的服务器URL配置

--- app/src/main/java/com/owen/packapk/MainActivity.java
+++ app/src/main/java/com/owen/packapk/MainActivity.java
@@ -2,17 +2,22 @@ package com.owen.packapk;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.owen.packapk.api.ApiContact;
 import com.owen.packapk.utils.LogUtil;
 import com.umeng.analytics.MobclickAgent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "packApk_MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LogUtil.d("这是一条Log日志");
+        Log.d(TAG, "server: " + ApiContact.BASE_URL);
     }
 
     @Override
---
+++ app/src/main/java/com/owen/packapk/api/ApiContact.java
@@ -0,0 +1,12 @@
+package com.owen.packapk.api;
+
+import com.owen.packapk.BuildConfig;
+
+/**
+ * Created by Owen on 2016/5/16.
+ */
+public class ApiContact {
+
+    public static final String BASE_URL = BuildConfig.SERVER_URL_DEBUG ? "开发环境" : "生产环境";
+
+}
